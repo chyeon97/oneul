6dac805642672515d3e5049410da00c3a3734313
chyeon97 <40657327+chyeon97@users.noreply.github.com>
2021-10-01 20:45:21 +0900

    Delete onuel-backend directory

--- onuel-backend/todolist/src/main/kotlin/com/onuel/todolist/controller/TodoRequest.kt
+++
@@ -1,5 +0,0 @@
-package com.onuel.todolist.controller
-
-import java.util.*
-
-data class TodoRequest (val todoName: String, val completed:Int, val dueDate: Date, val endDate: Date?)
--- onuel-backend/todolist/src/main/kotlin/com/onuel/todolist/repository/TodoRepository.kt
+++
@@ -1,5 +0,0 @@
-package com.onuel.todolist.repository
-
-import org.springframework.data.repository.CrudRepository
-
-interface TodoRepository : CrudRepository<Todo, Long>
